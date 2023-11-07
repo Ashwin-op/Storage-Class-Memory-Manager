633b51f81e3ff700dd08cc5b9abc7be124a87a62
Ashwin <bsashwin9@gmail.com>
2023-11-06 18:28:32 -0800

    Implement scm

--- avl.c
+++ avl.c
@@ -11,253 +11,290 @@
 #include "avl.h"
 
 struct avl {
-	struct state {
-		uint64_t items;
-		uint64_t unique;
-		struct node {
-			int depth;
-			uint64_t count;
-			const char *item;
-			struct node *left;
-			struct node *right;
-		} *root;
-	} *state; /* SCM */
-	struct scm *scm;
+    struct state {
+        uint64_t items;
+        uint64_t unique;
+        struct node {
+            int depth;
+            uint64_t count;
+            const char *item;
+            struct node *left;
+            struct node *right;
+        } *root;
+    } *state; /* SCM */
+    struct scm *scm;
 };
 
-static int
-delta(const struct node *node)
-{
-	return node ? node->depth : -1;
+static int delta(const struct node *node) {
+    return node ? node->depth : -1;
 }
 
-static int
-balance(const struct node *node)
-{
-	return delta(node->left) - delta(node->right);
+static int balance(const struct node *node) {
+    return delta(node->left) - delta(node->right);
 }
 
-static int
-depth(const struct node *a, const struct node *b)
-{
-	return (delta(a) > delta(b)) ? (delta(a) + 1) : (delta(b) + 1);
+static int depth(const struct node *a, const struct node *b) {
+    return (delta(a) > delta(b)) ? (delta(a) + 1) : (delta(b) + 1);
 }
 
-static struct node *
-rotate_right(struct node *node)
-{
-	struct node *root;
+static struct node *rotate_right(struct node *node) {
+    struct node *root;
 
-	root = node->left;
-	node->left = root->right;
-	root->right = node;
-	node->depth = depth(node->left, node->right);
-	root->depth = depth(root->left, node);
-	return root;
+    root = node->left;
+    node->left = root->right;
+    root->right = node;
+    node->depth = depth(node->left, node->right);
+    root->depth = depth(root->left, node);
+    return root;
 }
 
-static struct node *
-rotate_left(struct node *node)
-{
-	struct node *root;
+static struct node *rotate_left(struct node *node) {
+    struct node *root;
 
-	root = node->right;
-	node->right = root->left;
-	root->left = node;
-	node->depth = depth(node->left, node->right);
-	root->depth = depth(root->right, node);
-	return root;
+    root = node->right;
+    node->right = root->left;
+    root->left = node;
+    node->depth = depth(node->left, node->right);
+    root->depth = depth(root->right, node);
+    return root;
 }
 
-static struct node *
-rotate_left_right(struct node *node)
-{
-	node->left = rotate_left(node->left);
-	return rotate_right(node);
+static struct node *rotate_left_right(struct node *node) {
+    node->left = rotate_left(node->left);
+    return rotate_right(node);
 }
 
-static struct node *
-rotate_right_left(struct node *node)
-{
-	node->right = rotate_right(node->right);
-	return rotate_left(node);
+static struct node *rotate_right_left(struct node *node) {
+    node->right = rotate_right(node->right);
+    return rotate_left(node);
 }
 
-static struct node *
-update(struct avl *avl, struct node *root, const char *item)
-{
-	int d;
+static struct node *update(struct avl *avl, struct node *root, const char *item) {
+    int d;
 
-	if (!root) {
-		if (!(root = scm_malloc(avl->scm, sizeof (struct node)))) {
-			TRACE(0);
-			return NULL;
-		}
-		memset(root, 0, sizeof (struct node));
-		if (!(root->item = scm_strdup(avl->scm, item))) {
-			TRACE(0);
-			return NULL;
-		}
-		++root->count;
-		++avl->state->items;
-		++avl->state->unique;
-		return root;
-	}
-	if (!(d = strcmp(item, root->item))) {
-		++root->count;
-		++avl->state->items;
-	}
-	else if (0 > d) {
-		root->left = update(avl, root->left, item);
-		if (1 < abs(balance(root))) {
-			if (0 > strcmp(item, root->left->item)) {
-				root = rotate_right(root);
-			}
-			else {
-				root = rotate_left_right(root);
-			}
-		}
-	}
-	else if (0 < d) {
-		root->right = update(avl, root->right, item);
-		if (1 < abs(balance(root))) {
-			if (0 < strcmp(item, root->right->item)) {
-				root = rotate_left(root);
-			}
-			else {
-				root = rotate_right_left(root);
-			}
-		}
-	}
-	root->depth = depth(root->left, root->right);
-	return root;
+    if (!root) {
+        if (!(root = scm_malloc(avl->scm, sizeof(struct node)))) {
+            TRACE(0);
+            return NULL;
+        }
+        memset(root, 0, sizeof(struct node));
+        if (!(root->item = scm_strdup(avl->scm, item))) {
+            TRACE(0);
+            return NULL;
+        }
+        ++root->count;
+        ++avl->state->items;
+        ++avl->state->unique;
+        return root;
+    }
+    if (!(d = strcmp(item, root->item))) {
+        ++root->count;
+        ++avl->state->items;
+    } else if (0 > d) {
+        root->left = update(avl, root->left, item);
+        if (1 < abs(balance(root))) {
+            if (0 > strcmp(item, root->left->item)) {
+                root = rotate_right(root);
+            } else {
+                root = rotate_left_right(root);
+            }
+        }
+    } else {
+        root->right = update(avl, root->right, item);
+        if (1 < abs(balance(root))) {
+            if (0 < strcmp(item, root->right->item)) {
+                root = rotate_left(root);
+            } else {
+                root = rotate_right_left(root);
+            }
+        }
+    }
+    root->depth = depth(root->left, root->right);
+    return root;
 }
 
-static void
-traverse(struct node *node, avl_fnc_t fnc, void *arg)
-{
-	if (node) {
-		traverse(node->left, fnc, arg);
-		fnc(arg, node->item, node->count);
-		traverse(node->right, fnc, arg);
-	}
+static void traverse(struct node *node, avl_fnc_t fnc, void *arg) {
+    if (node) {
+        traverse(node->left, fnc, arg);
+        fnc(arg, node->item, node->count);
+        traverse(node->right, fnc, arg);
+    }
 }
 
-struct avl *
-avl_open(const char *pathname, int truncate)
-{
-	struct avl *avl;
+struct avl *avl_open(const char *pathname, int truncate) {
+    struct avl *avl;
 
-	assert( pathname );
+    assert(pathname);
 
-	if (!(avl = malloc(sizeof (struct avl)))) {
-		TRACE("out of memory");
-		return NULL;
-	}
-	memset(avl, 0, sizeof (struct avl));
-	if (!(avl->scm = scm_open(pathname, truncate))) {
-		avl_close(avl);
-		TRACE(0);
-		return NULL;
-	}
-	if (scm_utilized(avl->scm)) {
-		avl->state = scm_mbase(avl->scm);
-	}
-	else {
-		if (!(avl->state = scm_malloc(avl->scm,
-					      sizeof (struct state)))) {
-			avl_close(avl);
-			TRACE(0);
-			return NULL;
-		}
-		memset(avl->state, 0, sizeof (struct state));
-		assert( avl->state == scm_mbase(avl->scm) );
-	}
-	return avl;
+    if (!(avl = malloc(sizeof(struct avl)))) {
+        TRACE("out of memory");
+        return NULL;
+    }
+    memset(avl, 0, sizeof(struct avl));
+    if (!(avl->scm = scm_open(pathname, truncate))) {
+        avl_close(avl);
+        TRACE(0);
+        return NULL;
+    }
+    if (scm_utilized(avl->scm)) {
+        avl->state = scm_mbase(avl->scm);
+    } else {
+        if (!(avl->state = scm_malloc(avl->scm,
+                                      sizeof(struct state)))) {
+            avl_close(avl);
+            TRACE(0);
+            return NULL;
+        }
+        memset(avl->state, 0, sizeof(struct state));
+        assert(avl->state == scm_mbase(avl->scm));
+    }
+    return avl;
 }
 
-void
-avl_close(struct avl *avl)
-{
-	if (avl) {
-		scm_close(avl->scm);
-		memset(avl, 0, sizeof (struct avl));
-	}
-	FREE(avl);
+void avl_close(struct avl *avl) {
+    if (avl) {
+        scm_close(avl->scm);
+        memset(avl, 0, sizeof(struct avl));
+    }
+    FREE(avl);
 }
 
-int
-avl_insert(struct avl *avl, const char *item)
-{
-	struct node *root;
+int avl_insert(struct avl *avl, const char *item) {
+    struct node *root;
 
-	assert( avl );
-	assert( safe_strlen(item) );
+    assert(avl);
+    assert(safe_strlen(item));
 
-	if (!(root = update(avl, avl->state->root, item))) {
-		TRACE(0);
-		return -1;
-	}
-	avl->state->root = root;
-	return 0;
+    if (!(root = update(avl, avl->state->root, item))) {
+        TRACE(0);
+        return -1;
+    }
+    avl->state->root = root;
+    return 0;
 }
 
-uint64_t
-avl_exists(const struct avl *avl, const char *item)
-{
-	const struct node *node;
-	int d;
-
-	assert( avl );
-	assert( safe_strlen(item) );
-
-	node = avl->state->root;
-	while (node) {
-		if (!(d = strcmp(item, node->item))) {
-			return node->count;
-		}
-		node = (0 > d) ? node->left : node->right;
-	}
-	return 0;
+struct node *find_inorder_successor(struct node *node) {
+    struct node *current = node->right;
+    while (current && current->left != NULL) {
+        current = current->left;
+    }
+    return current;
 }
 
-void
-avl_traverse(const struct avl *avl, avl_fnc_t fnc, void *arg)
-{
-	assert( avl );
-	assert( fnc );
-
-	traverse(avl->state->root, fnc, arg);
+struct node *delete(struct avl *avl, struct node *root, const char *item) {
+    int d;
+    if (!root) {
+        return root;
+    }
+    if (!(d = strcmp(item, root->item))) {
+        if (root->count > 1) {
+            --root->count;
+            --avl->state->items;
+            return root;
+        }
+        if (!root->left && !root->right) {
+            scm_free(avl->scm, (void *) root->item);
+            scm_free(avl->scm, root);
+            --avl->state->items;
+            --avl->state->unique;
+            return NULL;
+        } else if (!root->left) {
+            struct node *temp = root->right;
+            scm_free(avl->scm, (void *) root->item);
+            scm_free(avl->scm, root);
+            --avl->state->items;
+            --avl->state->unique;
+            return temp;
+        } else if (!root->right) {
+            struct node *temp = root->left;
+            scm_free(avl->scm, (void *) root->item);
+            scm_free(avl->scm, root);
+            --avl->state->items;
+            --avl->state->unique;
+            return temp;
+        } else {
+            struct node *temp = find_inorder_successor(root);
+            scm_free(avl->scm, (void *) root->item);
+            root->item = scm_strdup(avl->scm, temp->item);
+            root->right = delete(avl, root->right, temp->item);
+        }
+    } else if (0 > d) {
+        root->left = delete(avl, root->left, item);
+    } else {
+        root->right = delete(avl, root->right, item);
+    }
+    if (1 < abs(balance(root))) {
+        if (0 > balance(root)) {
+            if (0 <= balance(root->left)) {
+                root = rotate_right(root);
+            } else {
+                root = rotate_left_right(root);
+            }
+        } else {
+            if (0 > balance(root->right)) {
+                root = rotate_left(root);
+            } else {
+                root = rotate_right_left(root);
+            }
+        }
+    }
+    root->depth = depth(root->left, root->right);
+    return root;
 }
 
-uint64_t
-avl_items(const struct avl *avl)
-{
-	assert( avl );
+int avl_remove(struct avl *avl, const char *item) {
+    assert(avl);
+    assert(safe_strlen(item));
 
-	return avl->state->items;
+    avl->state->root = delete(avl, avl->state->root, item);
+
+    return 0;
 }
 
-uint64_t
-avl_unique(const struct avl *avl)
-{
-	assert( avl );
+uint64_t avl_exists(const struct avl *avl, const char *item) {
+    const struct node *node;
+    int d;
 
-	return avl->state->unique;
+    assert(avl);
+    assert(safe_strlen(item));
+
+    node = avl->state->root;
+    while (node) {
+        if (!(d = strcmp(item, node->item))) {
+            return node->count;
+        }
+        node = (0 > d) ? node->left : node->right;
+    }
+    return 0;
 }
 
-size_t
-avl_scm_utilized(const struct avl *avl)
-{
-	assert( avl );
+void avl_traverse(const struct avl *avl, avl_fnc_t fnc, void *arg) {
+    assert(avl);
+    assert(fnc);
 
-	return scm_utilized(avl->scm);
+    traverse(avl->state->root, fnc, arg);
 }
 
-size_t
-avl_scm_capacity(const struct avl *avl)
-{
-	assert( avl );
+uint64_t avl_items(const struct avl *avl) {
+    assert(avl);
 
-	return scm_capacity(avl->scm);
+    return avl->state->items;
+}
+
+uint64_t avl_unique(const struct avl *avl) {
+    assert(avl);
+
+    return avl->state->unique;
+}
+
+size_t avl_scm_utilized(const struct avl *avl) {
+    assert(avl);
+
+    return scm_utilized(avl->scm);
+}
+
+size_t avl_scm_capacity(const struct avl *avl) {
+    assert(avl);
+
+    return scm_capacity(avl->scm);
 }
--- scm.c
+++ scm.c
@@ -29,3 +29,207 @@
  */
 
 /* research the above Needed API and design accordingly */
+
+#define HEAP_ADDR 0x600000000000
+
+#define META_HEAP_DATA_SIZE sizeof(size_t)
+#define META_BLOCK_BIT_SIZE sizeof(short)
+#define META_BLOCK_DATA_SIZE sizeof(size_t)
+#define META_BLOCK_SIZE (META_BLOCK_BIT_SIZE + META_BLOCK_DATA_SIZE)
+
+struct scm {
+    int fd;
+    struct {
+        size_t utilized;
+        size_t capacity;
+    } size;
+    void *addr;
+};
+
+struct scm *file_size(const char *pathname) {
+    struct stat st;
+    int fd;
+    struct scm *scm;
+
+    assert(pathname);
+
+    if (!(scm = malloc(sizeof(struct scm)))) {
+        return NULL;
+    }
+    memset(scm, 0, sizeof(struct scm));
+
+    if ((fd = open(pathname, O_RDWR)) == -1) {
+        free(scm);
+        TRACE("no such file");
+        return NULL;
+    }
+
+    if (fstat(fd, &st) == -1) {
+        free(scm);
+        close(fd);
+        return NULL;
+    }
+
+    if (!S_ISREG(st.st_mode)) {
+        free(scm);
+        close(fd);
+        return NULL;
+    }
+
+    scm->fd = fd;
+    scm->size.utilized = 0;
+    scm->size.capacity = st.st_size;
+
+    return scm;
+}
+
+struct scm *scm_open(const char *pathname, int truncate) {
+    struct scm *scm = file_size(pathname);
+    if (!scm) {
+        return NULL;
+    }
+
+    if (sbrk((long) scm->size.capacity) == (void *) -1) {
+        close(scm->fd);
+        free(scm);
+        return NULL;
+    }
+
+    if ((scm->addr = mmap((void *) HEAP_ADDR, scm->size.capacity, PROT_READ | PROT_WRITE, MAP_FIXED | MAP_SHARED,
+                          scm->fd, 0)) == MAP_FAILED) {
+        close(scm->fd);
+        free(scm);
+        return NULL;
+    }
+
+    if (truncate) {
+        if (ftruncate(scm->fd, (long) scm->size.capacity) == -1) {
+            close(scm->fd);
+            free(scm);
+            return NULL;
+        }
+        scm->size.utilized = 0;
+    } else {
+        scm->size.utilized = *(size_t *) scm->addr;
+    }
+
+    return scm;
+}
+
+void scm_close(struct scm *scm) {
+    assert(scm);
+
+    msync(scm->addr, scm->size.capacity, MS_SYNC);
+    munmap(scm->addr, scm->size.capacity);
+    close(scm->fd);
+
+    memset(scm, 0, sizeof(struct scm));
+    FREE(scm);
+}
+
+void *base_addr(struct scm *scm) {
+    return (char *) scm->addr + META_HEAP_DATA_SIZE;
+}
+
+void set_heap_meta(struct scm *scm) {
+    *(size_t *) scm->addr = scm->size.utilized;
+}
+
+void *block_addr(void *p) {
+    return (char *) p + META_BLOCK_SIZE;
+}
+
+short block_bit(void *p) {
+    return *(short *) p;
+}
+
+size_t block_size(void *p) {
+    return *(size_t *) ((char *) p + META_BLOCK_BIT_SIZE);
+}
+
+void *next_block_addr(void *p) {
+    return (char *) block_addr(p) + block_size(p);
+}
+
+void set_block_bit(void *p, short status) {
+    *(short *) p = status;
+}
+
+void set_block_size(void *p, size_t n) {
+    *(size_t *) ((char *) p + META_BLOCK_BIT_SIZE) = n;
+}
+
+void set_utilized(struct scm *scm, void *p, short increment) {
+    size_t delta = increment * (block_size(p) + META_BLOCK_SIZE);
+    scm->size.utilized += delta;
+
+    set_heap_meta(scm);
+}
+
+void *scm_malloc(struct scm *scm, size_t n) {
+    void *p = base_addr(scm);
+    void *end = (char *) scm->addr + scm->size.capacity;
+
+    while (p < end) {
+        if (block_bit(p) == 0) {
+            if ((char *) block_addr(p) + n <= (char *) end) {
+                set_block_bit(p, 2);
+                set_block_size(p, n);
+                set_utilized(scm, p, 1);
+                return block_addr(p);
+            } else {
+                break;
+            }
+        } else if (block_bit(p) == 1) {
+            if (block_size(p) >= n) {
+                set_block_bit(p, 2);
+                set_utilized(scm, p, 1);
+                return block_addr(p);
+            } else {
+                p = next_block_addr(p);
+            }
+        } else {
+            p = next_block_addr(p);
+        }
+    }
+
+    TRACE("no more space");
+    return NULL;
+}
+
+char *scm_strdup(struct scm *scm, const char *s) {
+    size_t n = strlen(s) + 1;
+    char *p;
+
+    if (!(p = scm_malloc(scm, n))) {
+        TRACE(0);
+        return NULL;
+    }
+    memcpy(p, s, n);
+
+    return p;
+}
+
+void scm_free(struct scm *scm, void *p) {
+    void *addr = (char *) p - META_BLOCK_SIZE;
+
+    if (block_bit(addr) == 2) {
+        set_block_bit(addr, 1);
+        memset(p, 0, block_size(addr));
+        set_utilized(scm, addr, -1);
+    } else {
+        TRACE("double free");
+    }
+}
+
+size_t scm_utilized(const struct scm *scm) {
+    return scm->size.utilized;
+}
+
+size_t scm_capacity(const struct scm *scm) {
+    return scm->size.capacity;
+}
+
+void *scm_mbase(struct scm *scm) {
+    return block_addr(base_addr(scm));
+}
